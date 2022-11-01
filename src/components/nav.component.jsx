bc35cbf7493d434af410f0b2765d593e1c3c1723
iMadfxq <imadfxq@gmail.com>
2022-11-01 04:14:36 -0400

    Video finished

--- src/components/nav.component.jsx
+++ src/components/nav.component.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Nav() {
   return (
     <>
     <nav>
-      <NavLink to="/home">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
+      <NavLink to="/home" className={({isActive}) => isActive ? 'grape' : undefined}>Home</NavLink>
+      <NavLink to="/about" style={({isActive}) => isActive ? {'font-size' : '5px'} : undefined}>About</NavLink>
       <NavLink to="/contact">Contact</NavLink>
     </nav>
     <Outlet/>
